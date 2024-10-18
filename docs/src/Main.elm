6df785778880912f0e017ac6030acf42b0fe86ee
Ken Stanton <this.tent@gmail.com>
2024-10-18 11:50:23 +0200

    Setting up the documentation page to work better.

--- docs/src/Main.elm
+++ docs/src/Main.elm
@@ -2,17 +2,44 @@ module Main exposing (main)
 
 -- import Html exposing (Html, div, text)
 
-import VirtualDom as Vd
+import Browser
+import Html exposing (Html)
+import Html.Attributes as Attr
 
 
-s : String -> String -> Vd.Attribute msg
+s : String -> String -> Html.Attribute msg
 s =
-    Vd.style
+    Attr.style
 
 
-main : Vd.Node ()
+main : Program () () ()
 main =
-    Vd.node "body"
+    Browser.document
+        { init = init
+        , update = update
+        , subscriptions = subs
+        , view =
+            \() ->
+                { title = "Dims: A Platform for Decentralized Research & Collaboration"
+                , body = [ view () ]
+                }
+        }
+
+
+init () =
+    ( (), Cmd.none )
+
+
+update () () =
+    ( (), Cmd.none )
+
+
+subs () =
+    Sub.none
+
+
+view () =
+    Html.node "body"
         [ s "background-color" "#000000"
         , s "color" "#ffffff"
         , s "width" "100vw"
@@ -20,14 +47,16 @@ main =
         , s "font-family" "monospace"
         , s "font-size" "1.5vmax"
         , s "line-height" "2.5vmax"
-        , s "padding" "2vmax"
         ]
-        [ Vd.text "Dims: A Platform for Decentralized Research & Collaboration"
-        , Vd.node "div"
-            [ s "font-size" "1vmax"
-            , s "padding-top" "0.5vmax"
-            , s "color" "#c8c8c8"
-            ]
-            [ Vd.text "- Documentation about this project will go here."
+        [ Html.div
+            [ s "padding" "2vmax" ]
+            [ Html.text "Dims: A Platform for Decentralized Research & Collaboration"
+            , Html.div
+                [ s "font-size" "1vmax"
+                , s "padding-top" "0.5vmax"
+                , s "color" "#c8c8c8"
+                ]
+                [ Html.text "- Documentation about this project will go here."
+                ]
             ]
         ]
